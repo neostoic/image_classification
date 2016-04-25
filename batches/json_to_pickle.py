e5d16bb5e3f3b3b5cd241244acc165090eaf8b84
rcamachobarranco <rcamachobarranco@utep.edu>
2016-04-25 13:01:26 -0600

    Added comments to json_to_pickle.py

--- batches/json_to_pickle.py
+++ batches/json_to_pickle.py
@@ -10,16 +10,19 @@ import sys
 
 from pandas.io.json import json_normalize
 
+# Create regex expression to properly parse JSON file created by Apache Drill.
 FLAGS = re.VERBOSE | re.MULTILINE | re.DOTALL
 WHITESPACE = re.compile(r'[ \t\n\r]*', FLAGS)
 
 
+# ConcatJSONDecoder subclasses the original decoder, to parse Apache Drill JSON
 class ConcatJSONDecoder(json.JSONDecoder):
     def decode(self, s, _w=WHITESPACE.match):
         s_len = len(s)
 
         objs = []
         end = 0
+        # Parse the JSON file removing whitespace with REGEX expression
         while end != s_len:
             obj, end = self.raw_decode(s, idx=_w(s, end).end())
             end = _w(s, end).end()
@@ -27,15 +30,22 @@ class ConcatJSONDecoder(json.JSONDecoder):
         return objs
 
 
+# Accept system arguments
 if len(sys.argv) > 1:
+    # First argument should be the file path of the JSON file
     filename = sys.argv[1]
+    # The pickled file will have the same name as the original, replacing .json with .pkl
     pkl_file = os.path.splitext(filename)[0] + '.pkl'
+    # Load the JSON data using the new decoder
     with open(filename) as f:
         json_data = json.load(f, cls=ConcatJSONDecoder)
+    # Use pandas normalize function to obtain a pandas dataframe (dictionary-style)
     df = json_normalize(json_data)
+    # Pickle pandas data frame
     df.to_pickle(pkl_file)
     print 'JSON data saved as' + pkl_file
 
+# If no arguments are introduced, try with the path defined below
 else:
     filename = 'C:\path\to\json'
     pkl_file = os.path.splitext(filename)[0] + '.pkl'
