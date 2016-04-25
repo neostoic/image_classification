12827efc8ff6aa16865b6f560a0e5c65be1455d2
rcamachobarranco <rcamachobarranco@utep.edu>
2016-04-25 13:49:07 -0600

    Added comments to add_business_id.py

--- captioning/add_business_id.py
+++ captioning/add_business_id.py
@@ -1,43 +1,56 @@
+# Helper script to add the business ID to the JSON files since this was not done initially
+# This is required to reference restaurants/photos
 import cPickle as pickle
 import json
 
 import pandas as pd
 
-# Load the caption data
+# Load the image data
 photos = pd.read_pickle(r'C:\Users\crobe\Google Drive\DataMiningGroup\Datasets\restaurant_photos_with_captions.pkl')
+
+# Load the image dataset where the business_id should be added
 dataset = json.load(
     open(r'C:\Users\crobe\Google Drive\DataMiningGroup\Datasets\image_caption_dataset_no_business.json'))
 dataset_images = dataset['images']
 
+# Use the query to find the record with the same photo id and add the business id to the other dataset
 for item in dataset_images:
     item['business_id'] = photos.query('photo_id == "' + item['yelpid'] + '"')['business_id'].iloc[0]
 
+# Store the new dataset that includes the business_id
 with open("C:\Users\crobe\Google Drive\DataMiningGroup\Datasets\image_caption_dataset.json", "w") as outfile:
+    # We use dump_dict to create the proper JSON structure
     dump_dict = dict()
     dump_dict['images'] = dataset_images
     json.dump(dump_dict, outfile)
 
+# Load next dataset where the business id should be added
 with open(
         r'C:\Users\crobe\Google Drive\DataMiningGroup\Datasets\caption_results\image_caption_with_predictions_coco.pkl',
         'rb') as f:
     dataset_images = pickle.load(f)
 
+# Use query to find the record with the same photo id and add the business id to the other dataset
 for item in dataset_images:
     item['business_id'] = photos.query('photo_id == "' + item['yelpid'] + '"')['business_id'].iloc[0]
 
+# Store the dataset
 with open("C:\Users\crobe\Google Drive\DataMiningGroup\Datasets\coco_caption_results.json", "w") as outfile:
     dump_dict = dict()
     dump_dict['images'] = dataset_images
     json.dump(dump_dict, outfile)
 
+# Open dataset
 with open(
         r'C:\Users\crobe\Google Drive\DataMiningGroup\Datasets\caption_results\image_caption_with_predictions_yelp.pkl',
         'rb') as f:
     dataset_images = pickle.load(f)
 
+# Use query to find the record with the same photo id and add the business id to the other dataset
 for item in dataset_images:
     item['business_id'] = photos.query('photo_id == "' + item['yelpid'] + '"')['business_id'].iloc[0]
 
+# Store the dataset
 with open("C:\Users\crobe\Google Drive\DataMiningGroup\Datasets\yelp_caption_results.json", "w") as outfile:
     dump_dict = dict()
     dump_dict['images'] = dataset_images
