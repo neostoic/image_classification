21048786ce3a985a105865dd5b5e90ab2e4d2649
rcamachobarranco <rcamachobarranco@utep.edu>
2016-05-05 23:07:20 -0600

    Finished commenting memload folder

--- memload/pickle_images.py
+++ memload/pickle_images.py
@@ -27,6 +27,7 @@ for k in range(1, batches):
     input_files.append(input_dir + 'train_batch' + str(k) + '.pkl')
 input_files.append(input_dir + 'test.pkl')
 
+
 def pickle_dataset(input_pkl, output_pkl, img_path, id_label, PIXELS):
     """
     This function takes the input pickle with text info and outputs a pickle with label and image only.
@@ -34,18 +35,23 @@ def pickle_dataset(input_pkl, output_pkl, img_path, id_label, PIXELS):
     data = pd.read_pickle(input_pkl)
     dataset = {}
 
+    # Initialize the arrays just for the first image
     iter_images = iter(data[id_label])
     first_image = next(iter_images)
     im = Image.open(img_path + first_image + '.jpg', 'r')
+    # Resize image
     im = ImageOps.fit(im, (PIXELS, PIXELS), Image.ANTIALIAS)
+    # Extract rgb channels
     im = (np.array(im))
     r = im[:, :, 0].flatten()
     g = im[:, :, 1].flatten()
     b = im[:, :, 2].flatten()
 
+    # Store image in a numpy array
     img_list = np.array(list(r) + list(g) + list(b), dtype='uint8')
     img_list = img_list[np.newaxis, :]
 
+    # Do the same for the rest of the images
     for img_name in iter_images:
         im = Image.open(img_path + img_name + '.jpg', 'r')
         im = ImageOps.fit(im, (PIXELS, PIXELS), Image.ANTIALIAS)
@@ -55,8 +61,10 @@ def pickle_dataset(input_pkl, output_pkl, img_path, id_label, PIXELS):
         b = im[:, :, 2].flatten()
 
         img = np.array(list(r) + list(g) + list(b), dtype='uint8')
+        # Stack vertically so that each row represents an image
         img_list = np.vstack((img_list, img[np.newaxis, :]))
 
+    # Use hickle to compress the data (pickle didn't work for this data type
     hkl.dump(img_list, output_pkl + '_data.hpy', mode='w', compression='gzip')
     hkl.dump(data['label'], output_pkl + '_labels.hpy', mode='w')
 
@@ -64,6 +72,7 @@ def pickle_dataset(input_pkl, output_pkl, img_path, id_label, PIXELS):
     del data
 
 
+# Pickle the datasets for different image sizes
 for pix in PIXELS:
     output_files = []
     for k in range(1, batches):
