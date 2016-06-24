a43aabf930d1cf05cc8777917b8d488bc27b4a20
BECCA\Rebecca <becca.urbi@gmail.com>
2016-06-24 00:13:32 -0600

    Updating site and files

--- autoCap/autoCap/models.py
+++ autoCap/autoCap/models.py
@@ -1,5 +1,11 @@
-# This is an auto-generated Django model module.
-# Models are the models found in the database.
+"""
+    Copyright 2005-2016 Django Software Foundation and individual contributors.
+    Django is a registered trademark of the Django Software Foundation.
+
+    This is an auto-generated Django model module.
+    Models are the models found in the database.
+
+"""
 
 from __future__ import unicode_literals
 
@@ -17,11 +23,32 @@ class Images(models.Model):
     predicted_label = models.TextField(blank=True, null=True)
     caption = models.TextField(blank=True, null=True)
     predicted_caption_1 = models.TextField(blank=True, null=True)
+    caption_confidence_1 = models.TextField(blank=True, null=True)
     predicted_caption_2 = models.TextField(blank=True, null=True)
+    caption_confidence_2 = models.TextField(blank=True, null=True)
     predicted_caption_3 = models.TextField(blank=True, null=True)
+    caption_confidence_3 = models.TextField(blank=True, null=True)
     predicted_caption_4 = models.TextField(blank=True, null=True)
+    caption_confidence_4 = models.TextField(blank=True, null=True)
     predicted_caption_5 = models.TextField(blank=True, null=True)
+    caption_confidence_5 = models.TextField(blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'images'
+
+class Reviews(models.Model):
+    review_id = models.TextField(blank=True, primary_key=True, serialize=False)
+    business_id = models.TextField(blank=True, null=True)
+    review_text = models.TextField(blank=True, null=True)
+    top_words = models.TextField(blank=True, null=True)
+    suggested_image1 = models.TextField(blank=True, null=True)
+    suggested_image2 = models.TextField(blank=True, null=True)
+    suggested_image3 = models.TextField(blank=True, null=True)
+    suggested_image4 = models.TextField(blank=True, null=True)
+    suggested_image5 = models.TextField(blank=True, null=True)
+    topic = models.TextField(blank=True, null = True)
+
+    class Meta:
+        managed = False
+        db_table = 'reviews'
--- autoCap/autoCap/urls.py
+++ autoCap/autoCap/urls.py
@@ -1,9 +1,12 @@
 # These are the url patterns per page
+#This was adapted from Simeon Franklin on August 13, 2015 to Udemy blog here:
+#https://blog.udemy.com/django-tutorial-getting-started-with-django/
+
 
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from views import index, tests, report
+from views import index, tests, report, nonelabel,suggest
 
 
 urlpatterns = [
@@ -11,4 +14,6 @@ urlpatterns = [
     url(r'^$', index),
     url(r'^tests.html', tests),
     url(r'^report.html', report),
+    url(r'^nonelabel.html', nonelabel),
+    url(r'^reviewSuggest.html', suggest),
 ]
