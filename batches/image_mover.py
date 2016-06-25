0919f437ec6f7d9292cce236baa53d28e18b0c2d
rcamachobarranco <rcamachobarranco@utep.edu>
2016-06-24 23:00:43 -0600

    Added googlenet model

--- batches/image_mover.py
+++ batches/image_mover.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import shutil
+import os
 
 
 def move_images(path_photos):
+    if not os.path.exists(r'./data/restaurant_photos/'):
+        os.makedirs(r'./data/restaurant_photos/')
     photos = pd.read_pickle(r'./data/restaurant_photos.pkl')
 
     for idx, photo_id in enumerate(photos.photo_id):
-        path_in = r'./data/restaurant_photos/' + photo_id + '.jpg'
-        path_out = path_photos + photos.label[idx] + r'/' + photo_id + '.jpg'
+        path_out = r'./data/restaurant_photos/' + photo_id + '.jpg'
+        path_in = path_photos + r'/' + photo_id + '.jpg'
         shutil.copy(path_in, path_out)
--- framework.py
+++ framework.py
@@ -1,3 +1,4 @@
+import os
 from multiprocessing import freeze_support
 from batches.image_mover import move_images
 from batches.pickle_train_test_data import create_train_test_data
@@ -8,21 +9,13 @@ from captioning.preprocess_db import preprocess_db
 from captioning.train_rnn import train_rnn
 
 MODELS = ['VGG_16', 'VGG_19', 'googlenet', 'inception_v3']
-#home/
-#D:
-#C:
-# Paths that we must take into account!
-#categories.pkl
-#cnn_features = r'./data/image_caption_with_cnn_features.pkl'
-#'results_yelp_webpage.json'
-#'captions_yelp_nic_results_webpage.json'
-# Check add business id
 
 if __name__ == '__main__':
     freeze_support()
     # HARDCODED: CHANGE THIS TO THE PATH WHERE THE PHOTOS ARE DOWNLOADED:
-    path_photos = r'D:/Yelp/Photos/'
-    move_images(path_photos)
+    path_photos = r'D:/Yelp/restaurant_photos/'
+    if not os.path.exists(r'./data/restaurant_photos/'):
+        move_images(path_photos)
     create_train_test_data(folds=6)
     train('googlenet')
     preprocess_db()
