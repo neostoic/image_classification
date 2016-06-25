2eb7a84d72e844418032e02347d4bf7dd6efdeb5
rcamachobarranco <rcamachobarranco@utep.edu>
2016-06-24 22:40:15 -0600

    Fixes to paths

--- framework.py
+++ framework.py
@@ -8,20 +8,11 @@ from captioning.preprocess_db import preprocess_db
 from captioning.train_rnn import train_rnn
 
 MODELS = ['VGG_16', 'VGG_19', 'googlenet', 'inception_v3']
-#home/
-#D:
-#C:
-# Paths that we must take into account!
-#categories.pkl
-#cnn_features = r'./data/image_caption_with_cnn_features.pkl'
-#'results_yelp_webpage.json'
-#'captions_yelp_nic_results_webpage.json'
-# Check add business id
 
 if __name__ == '__main__':
     freeze_support()
     # HARDCODED: CHANGE THIS TO THE PATH WHERE THE PHOTOS ARE DOWNLOADED:
-    path_photos = r'D:/Yelp/Photos/'
+    path_photos = r'D:/Yelp/restaurant_photos/'
     move_images(path_photos)
     create_train_test_data(folds=6)
     train('googlenet')
@@ -29,4 +20,3 @@ if __name__ == '__main__':
     get_labels_features()
     train_rnn()
     captioning()
-
